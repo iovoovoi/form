149139e595ae573f9ec004357cbc11c5c5753d92
iovoovoi <erinababie@gmail.com>
2018-10-18 18:06:10 +1100

    safsad

--- js/form.js
+++ js/form.js
@@ -15,6 +15,15 @@ function setRadio(t, element) {
     t.classList.add('btn-active');
 }
 /**
+ * 检查手机格式
+ * @return {bool} 
+ */
+function checkMobile(val) {
+    if (val.search(/^04\d{8}$/) == -1) {
+        return false;
+    }
+}
+/**
  * ajax提交表单
  * code:
  * 100  业务正常
@@ -22,25 +31,33 @@ function setRadio(t, element) {
  * @return {mixed} {data:([] || object || null),code:integer,msg:string}
  */
 function appointment() {
+    //check mobile number
+    if (checkMobile($('#phone').val()) == false) {
+        alert('手机号码格式不符合');
+        return;
+    }
     //disable complete button,不允许重复提交
     $('.complete').addClass('btn-disable');
     $('.complete').attr('disabled', 'disabled');
-
     //提交ajax
     var data = $('#form').serialize();
-    console.log(data);
     $.ajax({
-        'url': 'https://rocky-mesa-30908.herokuapp.com/request',
+        'url': 'test.php',
         'type': 'POST',
         'dataType': "json",
+        'timeout': 2000,
         'data': data,
         'success': function(result) {
+            //不管结果直接跳转
+            var request_type = $('#request_type').val();
+            request_type = request_type == 1 ? 1 : 0;
+            location.href = 'success.html?request_type=' + request_type;
+            return;
             if (result.code == 100) {
                 //成功跳转
-                location.href = 'success.html';
+                location.href = 'success.html?request_type=' + request_type;
             } else {
-                //
-             失败看console
+                //失败看console
                 console.log('ajax fail');
                 console.log('data:');
                 console.log(data);
